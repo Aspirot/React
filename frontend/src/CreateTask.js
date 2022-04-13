097a699092e6e13ce64b1dab15109b8d93e4db9b
Aspirot <81372169+Aspirot@users.noreply.github.com>
2022-04-13 14:18:05 -0400

    create finished?

--- frontend/src/CreateTask.js
+++ frontend/src/CreateTask.js
@@ -1,42 +1,56 @@
-import { useState } from "react"
+import axios from "axios"
+import { useEffect, useState } from "react"
 
 
 export default function CreateTask(){
     const [title,setTitle] = useState('')
     const [description,setDescription] = useState('')
     const [status,setStatus] = useState("In Progress")
+    const [hasCreated,setHasCreated] = useState(false)
     const statusOptions = ["In Progress","Completed"]
 
-
     function submit(){
-        console.log(title + "  " + description + "  " + status)
+        if(title && description && status)
+            addTask()
+        else
+            console.log("Give all the information on the task")
+    }
+
+    async function addTask(){
+        const baseURL = 'http://localhost:8082/tasks/addTask';
+        const newTask = { title:title,description:description,status:status};
+        await axios.post(`${baseURL}`,newTask)
+        .then(() => {setHasCreated(true);console.log(newTask)})
+        setTitle('')
+        setDescription('')
+        setStatus("In Progress")
     }
 
     return(
         <div>
             <h1>Create a task</h1>
-
-                <label>
-                    Title : 
-                    <input type="text" value={title} onChange={event => setTitle(event.target.value)}/>
-                </label>
-                <br/>
-                <label>
-                    Description : 
-                    <input type="text" value={description} onChange={event => setDescription(event.target.value)} />
-                </label>
-                <br/>
-                <label>
-                    Status : 
-                    <select type="text" value={status} onChange={event => setStatus(event.target.value)}>
-                        {statusOptions.map(o => (
-                            <option value={o} key={o}>{o}</option>
-                        ))}
-                    </select>
-                </label>
-                <br/>
-                <button type="submit" onClick={submit}>Submit</button>
-
+            <label>
+                Title : 
+                <input type="text" value={title} onChange={event => setTitle(event.target.value)}/>
+            </label>
+            <br/>
+            <label>
+                Description : 
+                <input type="text" value={description} onChange={event => setDescription(event.target.value)} />
+            </label>
+            <br/>
+            <label>
+                Status : 
+                <select type="text" value={status} onChange={event => setStatus(event.target.value)}>
+                    {statusOptions.map(o => (
+                        <option value={o} key={o}>{o}</option>
+                    ))}
+                </select>
+            </label>
+            <br/>
+            <button type="submit" onClick={submit}>Submit</button>
+            {hasCreated && 
+            <h3>Task {title} has been created!</h3>}
         </div>
     )
 }
